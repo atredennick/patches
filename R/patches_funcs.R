8078968d24cbb612991ec7ebe75fda3c30d47533
Andrew Tredennick <atredenn@gmail.com>
2015-10-15 16:20:31 -0600

    adds working version of individual functions

--- R/patches_funcs.R
+++ R/patches_funcs.R
@@ -110,8 +110,11 @@ ind_spp_var <- function(D,
   Dtmp <- D[,c("id_var", "time_var", species_columns)]
   Dmelt <- melt(Dtmp, id.vars = c("id_var", "time_var"))
   Dout <- ddply(Dmelt, .(id_var, variable), summarise,
+                avg = mean(value),
+                sdev = sd(value),
                 cv = sd(value)/mean(value))
-  colnames(Dout) <- c("plot", "species", "cv")
+  colnames(Dout) <- c("plot", "species", "avg", "sdev", "cv")
+  Dout <- Dout[which(Dout$avg>0),]
   return(Dout)
   
 } # end function
@@ -160,29 +163,36 @@ avg_spp_var <- function(D,
   species_columns <- colnames(D)[grep(species_id, colnames(D))]
   species_columns <- species_columns[-grep("species", species_columns)]
   
+  
   ####
   ####  Variability of j-th species in patch i (CV_j(i)^2)
   ####
-  avgd_spp_var <- numeric(num_plots)
-  for(i in 1:num_plots){
-    tmp_data <- subset(D, id_var==plot_ids[i])
-    comm_data <- tmp_data[,species_columns]
-    # Remove species columns that only include 0s
-    keepers <- which(colSums(comm_data)!=0)
-    comm_data <- comm_data[,keepers]
-    cv_ji <- apply(comm_data, MARGIN = 2, sd)/apply(comm_data, MARGIN = 2, mean)
-    cv_df <- data.frame(species=colnames(comm_data),
-                        cv = cv_ji)
-    spp_within_plot_cv[[as.character(plot_ids[i])]] <- cv_df
-    
-    avg_spp_biom <- as.numeric(apply(comm_data, MARGIN = 2, mean))
-    avg_plot_biom <- mean(as.matrix(comm_data))
-    
-    avgd_spp_var[i] <- sum(avg_spp_biom / avg_plot_biom * cv_ji)
-    
-  } # end plots loop
+  Dtmp <- D[,c("id_var", "time_var", species_columns)]
+  Dmelt <- melt(Dtmp, id.vars = c("id_var", "time_var"))
+  Dout <- ddply(Dmelt, .(id_var, variable), summarise,
+                avg = mean(value),
+                sdev = sd(value),
+                cv = sd(value)/mean(value))
+  colnames(Dout) <- c("plot", "species", "avg", "sdev", "cv")
+  Dout <- Dout[which(Dout$avg>0),] #remove spp absent throughout time series
   
-  output_df <- data.frame(plot=plot_ids, avgd_spp_var=avgd_spp_var)
+  # Get mean community biomass per plot
+  Dplot <- Dmelt[which(Dmelt$value>0),]
+  plot_biomass <- ddply(Dplot, .(id_var, time_var), summarise,
+                        tot_plot_biom = sum(value))
+  avg_plot_biomass <- ddply(plot_biomass, .(id_var), summarise,
+                             avg_biomass = mean(tot_plot_biom))
+  names(avg_plot_biomass) <- c("plot", "avg_plot_biomass")
+  
+  Dout <- merge(Dout, avg_plot_biomass) 
+  
+  # Make spp weights
+  Dout$sppweights <- with(Dout, avg/avg_plot_biomass)
+  Dout$var <- with(Dout, sppweights*cv)
+  
+  output_df <- ddply(Dout, .(plot), summarise,
+                     sum(var))
+  names(output_df) <- c("plot", "avgd_spp_var")
   return(output_df)
   
 } # end function
@@ -193,14 +203,14 @@ avg_spp_var <- function(D,
 
 
 
-#' Variability of community (plot) i
+#' Variability of community plot i
 #' @author Andrew Tredennick
 #' @param D Dataframe with time series of species level measurements of abundance/biomass across multiple patches. The dataframe can contain many additional columns that will be ignored.
 #' @param id_var Column name within dataframe that identifies unique patches. Defaults to "plot_id".
 #' @param time_var Column name within dataframe for the temporal identifier. Defaults to "calendar_year".
 #' @param species_id Character flag for columns indicating species id. Defaults to "sp".
 #' @return output_df Dataframe of community variability for each plot; CV^2.
-avg_spp_var <- function(D, 
+community_var <- function(D, 
                         id_var="plot_id", 
                         time_var="calendar_year", 
                         species_id="sp"){
@@ -258,25 +268,31 @@ avg_spp_var <- function(D,
   ####
   ####  Variability of j-th species in patch i (CV_j(i)^2)
   ####
-  avgd_spp_var <- numeric(num_plots)
-  for(i in 1:num_plots){
-    tmp_data <- subset(D, id_var==plot_ids[i])
-    comm_data <- tmp_data[,species_columns]
-    # Remove species columns that only include 0s
-    keepers <- which(colSums(comm_data)!=0)
-    comm_data <- comm_data[,keepers]
-    cv_ji <- apply(comm_data, MARGIN = 2, sd)/apply(comm_data, MARGIN = 2, mean)
-    cv_df <- data.frame(species=colnames(comm_data),
-                        cv = cv_ji)
-    spp_within_plot_cv[[as.character(plot_ids[i])]] <- cv_df
-    
-    avg_spp_biom <- as.numeric(apply(comm_data, MARGIN = 2, mean))
-    avg_plot_biom <- mean(as.matrix(comm_data))
-    
-    avgd_spp_var[i] <- sum(avg_spp_biom / avg_plot_biom * cv_ji)
-    
-  } # end plots loop
+  Dtmp <- D[,c("id_var", "time_var", species_columns)]
+  Dmelt <- melt(Dtmp, id.vars = c("id_var", "time_var"))
+  Dout <- ddply(Dmelt, .(id_var, variable), summarise,
+                avg = mean(value),
+                sdev = sd(value),
+                cv = sd(value)/mean(value))
+  colnames(Dout) <- c("plot", "species", "avg", "sdev", "cv")
+  Dout <- Dout[which(Dout$avg>0),] #remove spp absent throughout time series
   
+  # Get mean community biomass per plot
+  Dplot <- Dmelt[which(Dmelt$value>0),]
+  plot_biomass <- ddply(Dplot, .(id_var, time_var), summarise,
+                        tot_plot_biom = sum(value))
+  avg_plot_biomass <- ddply(plot_biomass, .(id_var), summarise,
+                            avg_biomass = mean(tot_plot_biom))
+  names(avg_plot_biomass) <- c("plot", "avg_plot_biomass")
+  
+  Dout <- merge(Dout, avg_plot_biomass) 
+  
+  # Make spp weights
+  Dout$sppweights <- with(Dout, avg/avg_plot_biomass)
+  Dout$var <- with(Dout, sppweights*cv)
+  var_df <- ddply(Dout, .(plot), summarise,
+                     sum(var))
+  avgd_spp_var <- var_df[,2]
   
   var_comm <- (avgd_spp_var^2)*species_synchrony[,2]
   output_df <- data.frame(plot=plot_ids, var_comm_sq=var_comm)
@@ -296,7 +312,7 @@ avg_spp_var <- function(D,
 #' @param time_var Column name within dataframe for the temporal identifier. Defaults to "calendar_year".
 #' @param species_id Character flag for columns indicating species id. Defaults to "sp".
 #' @return output Numeric scalar for alpha variability (CV) at local scale.
-avg_spp_var <- function(D, 
+alpha_var <- function(D, 
                         id_var="plot_id", 
                         time_var="calendar_year", 
                         species_id="sp"){
@@ -354,33 +370,40 @@ avg_spp_var <- function(D,
   ####
   ####  Variability of j-th species in patch i (CV_j(i)^2)
   ####
-  avgd_spp_var <- numeric(num_plots)
-  plot_avgs <- numeric(num_plots)
-
-  for(i in 1:num_plots){
-    tmp_data <- subset(D, id_var==plot_ids[i])
-    comm_data <- tmp_data[,species_columns]
-    # Remove species columns that only include 0s
-    keepers <- which(colSums(comm_data)!=0)
-    comm_data <- comm_data[,keepers]
-    cv_ji <- apply(comm_data, MARGIN = 2, sd)/apply(comm_data, MARGIN = 2, mean)
-    cv_df <- data.frame(species=colnames(comm_data),
-                        cv = cv_ji)
-    spp_within_plot_cv[[as.character(plot_ids[i])]] <- cv_df
-    
-    avg_spp_biom <- as.numeric(apply(comm_data, MARGIN = 2, mean))
-    avg_plot_biom <- mean(as.matrix(comm_data))
-    
-    avgd_spp_var[i] <- sum(avg_spp_biom / avg_plot_biom * cv_ji)/num_plots
-    plot_avgs[i] <- avg_plot_biom
-    
-  } # end plots loop
+  Dtmp <- D[,c("id_var", "time_var", species_columns)]
+  Dmelt <- melt(Dtmp, id.vars = c("id_var", "time_var"))
+  Dout <- ddply(Dmelt, .(id_var, variable), summarise,
+                avg = mean(value),
+                sdev = sd(value),
+                cv = sd(value)/mean(value))
+  colnames(Dout) <- c("plot", "species", "avg", "sdev", "cv")
+  Dout <- Dout[which(Dout$avg>0),] #remove spp absent throughout time series
   
-  plot_weights <- plot_avmean(plot_avgs)
-  var_comm <- sqrt((avgd_spp_var^2)*species_synchrony[,2])
-  alpha_var <- (sum(var_comm*plot_weights))^2
+  # Get mean community biomass per plot
+  Dplot <- Dmelt[which(Dmelt$value>0),]
+  plot_biomass <- ddply(Dplot, .(id_var, time_var), summarise,
+                        tot_plot_biom = sum(value))
+  avg_plot_biomass <- ddply(plot_biomass, .(id_var), summarise,
+                            avg_biomass = mean(tot_plot_biom))
+  names(avg_plot_biomass) <- c("plot", "avg_plot_biomass")
   
-  return(alpha_var)
+  Dout <- merge(Dout, avg_plot_biomass) 
+  
+  # Make spp weights
+  Dout$sppweights <- with(Dout, avg/avg_plot_biomass)
+  Dout$var <- with(Dout, sppweights*cv)
+  var_df <- ddply(Dout, .(plot), summarise,
+                  sum(var))
+  avgd_spp_var <- var_df[,2]
+  var_comm_sq <- (avgd_spp_var^2)*species_synchrony[,2]
+  
+  plot_weights <- avg_plot_biomass$avg_plot_biomass/mean(avg_plot_biomass$avg_plot_biomass)
+  var_comm_sq <- (avgd_spp_var^2)*species_synchrony[,2]
+  
+  
+  alpha_cv_sq <- (sum(plot_weights*sqrt(var_comm_sq)))^2
+  
+  return(alpha_cv_sq)
   
 } # end function
 
@@ -389,23 +412,81 @@ avg_spp_var <- function(D,
 
 
 
-
-
-
-#' Calculate alpha, beta, and gamma variability metrics from community matrix
+#' Calculate beta variability; 1/spatial synchrony
 #' @author Andrew Tredennick
 #' @param D Dataframe with time series of species level measurements of abundance/biomass across multiple patches. The dataframe can contain many additional columns that will be ignored.
 #' @param id_var Column name within dataframe that identifies unique patches. Defaults to "plot_id".
 #' @param time_var Column name within dataframe for the temporal identifier. Defaults to "calendar_year".
 #' @param species_id Character flag for columns indicating species id. Defaults to "sp".
-
-patches_var <- function(D, 
-                        id_var="plot_id", 
-                        time_var="calendar_year", 
-                        species_id="sp"){
+#' @return Numeric scalar of beta variability
+beta_var <- function(D,
+                     id_var="plot_id", 
+                     time_var="calendar_year", 
+                     species_id="sp"){
   require(plyr)
   require(dplyr)
-  require(synchrony)
+  
+  ####
+  ##  Housekeeping for column names
+  ####
+  colnames(D)[which(colnames(D)==id_var)] <- "id_var"
+  colnames(D)[which(colnames(D)==time_var)] <- "time_var"
+  num_plots <- length(unique(D$id_var))
+  plot_ids <- unique(D$id_var)
+  
+  ####
+  ##  Test to make sure plots occur at all time points
+  ####
+  yrs <- unique(D$time_var)
+  yr_plots <- list()
+  for(y in 1:length(yrs)){
+    tmp <- subset(D, time_var==yrs[y])
+    yr_plots <- unique(tmp$id_var)
+    if(all(plot_ids %in% yr_plots)==FALSE){
+      stop("plot numbers are uneven through years; \nconsider breaking up the analysis by years")
+    }
+  }
+
+  
+  species_columns <- colnames(D)[grep(species_id, colnames(D))]
+  species_columns <- species_columns[-grep("species", species_columns)]
+  
+  spp_long <- melt(D, measure.vars = species_columns)
+  patch_abund <- ddply(spp_long, .(time_var, id_var), summarise,
+                       comm_anpp = sum(value))
+  patch_cvs <- ddply(patch_abund , .(id_var), summarise,
+                     cv = sd(comm_anpp)/mean(comm_anpp),
+                     sds = sd(comm_anpp))
+  total_metacomm <- ddply(spp_long, .(time_var), summarise,
+                          total_biomass = sum(value))
+  mean_metacomm <- mean(total_metacomm$total_biomass)
+  weighted_local_cv <- sum(patch_cvs$sds) / mean_metacomm
+  
+  comm_matrix <- dcast(patch_abund, time_var~id_var, value.var="comm_anpp")
+  w <- cov(comm_matrix[,2:ncol(comm_matrix)])
+  metacomm_cv <- sqrt(sum(w))/mean_metacomm
+  patch_synchrony <- metacomm_cv^2 / weighted_local_cv^2
+  
+  return(1/patch_synchrony)
+} # end beta var function
+
+
+
+
+
+#' Calculate gamma variability
+#' @author Andrew Tredennick
+#' @param D Dataframe with time series of species level measurements of abundance/biomass across multiple patches. The dataframe can contain many additional columns that will be ignored.
+#' @param id_var Column name within dataframe that identifies unique patches. Defaults to "plot_id".
+#' @param time_var Column name within dataframe for the temporal identifier. Defaults to "calendar_year".
+#' @param species_id Character flag for columns indicating species id. Defaults to "sp".
+#' @return Numeric scalar of gamma variability 
+gamma_var <- function(D, 
+                      id_var="plot_id", 
+                      time_var="calendar_year", 
+                      species_id="sp"){
+  require(plyr)
+  require(dplyr)
   
   ####
   ##  Housekeeping for column names
@@ -428,85 +509,26 @@ patches_var <- function(D,
     }
   }
   
-  ####
-  ##  Within patch species synchrony
-  ####
   species_columns <- colnames(D)[grep(species_id, colnames(D))]
   species_columns <- species_columns[-grep("species", species_columns)]
-  species_synchrony <- matrix(ncol=2, nrow=num_plots)
-  for(plot_now in 1:num_plots){
-    tmp_data <- subset(D, id_var==plot_ids[plot_now])
-    comm_matrix <- tmp_data[,species_columns]
-    species_synchrony[plot_now,1] <- plot_ids[plot_now]
-    species_synchrony[plot_now,2] <- as.numeric(community.sync(comm_matrix)[1])
-  }
   
-  
-  ####
-  ##  Species level variability within local patches: squared coefficient of 
-  ##  temporal variation of species biomass
-  ####
-  Dtmp <- D[,c("id_var", "time_var", species_columns)]
-  Dmelt <- melt(Dtmp, id.vars = c("id_var", "time_var"))
-  species_cvs <- ddply(Dmelt, .(id_var, variable), summarise,
-                       cv_square = (sd(value)/mean(value))^2)
-  species_cvs <- species_cvs[complete.cases(species_cvs),]
-  ##TODO: calculate weighted average species CV across patches???
-  
-  
-  ####
-  ##  Patch level richness and evenness, averaged over time
-  ####
-  Dtmp <- D[,c("id_var", "time_var", species_columns)]
-  Dmelt <- melt(Dtmp, id.vars = c("id_var", "time_var"))
-  species_abundance <- ddply(Dmelt, .(id_var, time_var), summarise,
-                             abundance = length(which(value>0)))
-  avg_spp_rich <- ddply(species_abundance, .(id_var), summarise,
-                        avg_richness = mean(abundance))
-  ##TODO: add in species evenness metric
-  
-  
-  ####
-  ##  Patch synchrony and CVs
-  ####
   spp_long <- melt(D, measure.vars = species_columns)
   patch_abund <- ddply(spp_long, .(time_var, id_var), summarise,
-                     comm_anpp = sum(value))
+                       comm_anpp = sum(value))
   patch_cvs <- ddply(patch_abund , .(id_var), summarise,
                      cv = sd(comm_anpp)/mean(comm_anpp),
                      sds = sd(comm_anpp))
   total_metacomm <- ddply(spp_long, .(time_var), summarise,
                           total_biomass = sum(value))
-  mean_metacomm <- mean(total_metacomm$total_biomass)
-  weighted_local_cv <- sum(patch_cvs$sds) / mean_metacomm
   
-  comm_matrix <- dcast(patch_abund, time_var~id_var, value.var="comm_anpp")
-  w <- cov(comm_matrix[,2:ncol(comm_matrix)])
-  metacomm_cv <- sqrt(sum(w))/mean_metacomm
-  patch_synchrony <- metacomm_cv^2 / weighted_local_cv^2
-  
-  
-  ####
-  ##  Gamma (regional) variability
-  ####
-  gamma_var <- sd(total_metacomm$total_biomass)/mean(total_metacomm$total_biomass)
-  gamma_test <- weighted_local_cv^2 / (1/patch_synchrony)
-  if(round(gamma_var^2,2) != round(gamma_test,2)){
+  gamma_cv <- sd(total_metacomm$total_biomass)/mean(total_metacomm$total_biomass)
+  alpha_variability <- alpha_var(D = D)
+  beta_variability <- beta_var(D = D)
+  gamma_test <- alpha_variability / beta_variability
+  if(round(gamma_cv^2,2) != round(gamma_test,2)){
     warning("alpha and beta variability not producing gamma variability as expected")
   }
   
-  ####
-  ##  Collate output
-  ####
-  out_list <- list(species_synchrony = species_synchrony,
-                   species_var = species_cvs,
-                   plot_species_richness = avg_spp_rich,
-                   weighted_patch_cv = weighted_local_cv,
-                   alpha_var = weighted_local_cv^2,
-                   patch_synchrony = patch_synchrony,
-                   beta_var = 1/patch_synchrony,
-                   number_of_patches = num_plots,
-                   gamma_var = gamma_var^2)
-  return(out_list)
-}
+  return(gamma_test)
 
+} # end gamma variability function
